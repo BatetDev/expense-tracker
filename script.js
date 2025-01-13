1c37b8c86d5858261567dc89964c3a1d873b7b31
fr4nbtt <batet.francisco@gmail.com>
2025-01-13 18:39:33 -0300

    init: add DOM selectors and initial transaction data

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const balance = document.querySelector("#balance");
+const money_plus = document.querySelector("#money-plus");
+const money_minus = document.querySelector("#money_minus");
+const list = document.querySelector("#list");
+const form = document.querySelector("#form");
+const text = document.querySelector("#text");
+const amount = document.querySelector("#amount");
+
+const dummyTransactions = [
+  { id: 1, text: "Flower", amount: -20 },
+  { id: 2, text: "Salary", amount: 300 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 },
+];
+
+let transactions = dummyTransactions;
