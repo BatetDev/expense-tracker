1e8a62dd23549473f0072d78f3bda4b8b7bf4e9b
fr4nbtt <batet.francisco@gmail.com>
2025-01-13 23:12:57 -0300

    feat: add transactions history persistance with localStorage

--- script.js
+++ script.js
@@ -8,14 +8,12 @@ const form = document.querySelector("#form");
 const text = document.querySelector("#text");
 const amount = document.querySelector("#amount");
 
-const dummyTransactions = [
-  { id: 1, text: "Flower", amount: -20 },
-  { id: 2, text: "Salary", amount: 300 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 4, text: "Camera", amount: 150 },
-];
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
 
-let transactions = dummyTransactions;
+let transactions =
+  localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
 
 // Add transaction
 function addTransaction(e) {
@@ -36,6 +34,8 @@ function addTransaction(e) {
     addTransactionDOM(transaction);
 
     updateValues();
+
+    updateLocalStorage();
 
     text.value = "";
     amount.value = "";
@@ -93,7 +93,14 @@ function updateValues() {
 function removeTransaction(id) {
   transactions = transactions.filter((transaction) => transaction.id !== id);
 
+  updateLocalStorage();
+
   init();
+}
+
+// Update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
 }
 
 // Init app
